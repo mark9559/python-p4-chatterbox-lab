cde36bf76d326d42c694a087814146b02b509602
mark9559 <mwangimark57@gmail.com>
2024-01-10 23:29:18 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -14,13 +14,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET'])
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    serialized_messages = [message.serialize() for message in messages]
+    return jsonify(serialized_messages)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['GET'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.get_or_404(id)
+    return jsonify(message.serialize())
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.json
+    if 'body' not in data or 'username' not in data:
+        return jsonify({'error': 'Please provide body and username'}), 400
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.serialize()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.json
+    if 'body' in data:
+        message.body = data['body']
+    db.session.commit()
+    return jsonify(message.serialize())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({'message': 'Message deleted successfully'})
 
 if __name__ == '__main__':
     app.run(port=5555)
